243473476a2c5aa09a1f18a7bc4a83e9a779e4d0
bax082024 <bax082024@gmail.com>
2024-11-05 17:19:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpMethodsExample
+{
+  class Program
+  {
+
+    
+  }
+
+}
